154f2fa5c4e55047ed955eebe6e229196672c92d
Satyarth <srk.satyarth.1@gmail.com>
2021-08-27 01:59:54 +0530

    Custom JWT-auth

---
+++ auth_system/authentication/custom_auth/generate_token.py
@@ -0,0 +1,54 @@
+"""
+This file has the module for generating Auth and Refresh token, respectively
+"""
+
+import datetime
+import jwt
+from django.conf import settings
+
+
+def generate_auth_token(user):
+    """
+    Auth tokens carry the necessary information to access a resource directly.
+    In other words, when a client passes an access token to a server managing
+        a resource, that server can use the information contained in the token
+        to decide whether the client is authorized or not.
+    """
+
+    # auth token payload which expires in 5 minutes
+    auth_token_payload = {
+        "user_id": user.id,
+        "exp": datetime.datetime.utcnow() +
+        datetime.timedelta(days=0, minutes=5),
+        "iat": datetime.datetime.utcnow(),
+    }
+    # generating auth token using HS256 algorithm and then encoding it
+    # with the jwt lib using the django secret key
+    auth_token = jwt.encode(
+        auth_token_payload, settings.SECRET_KEY, algorithm="HS256"
+    )
+    return auth_token
+
+
+def generate_refresh_token(user):
+    """
+    The idea of refresh tokens is that if an access token is compromised,
+    because it is short-lived, the attacker has a limited window to abuse it.
+
+    Refresh tokens carry the information necessary to get a new access token.
+    In other words, whenever an access token is required to access a specific
+    resource, a client may use a refresh token to get a new access token issued
+    by the authentication server.
+    """
+
+    # refresh token which expires in 5 days.
+    refresh_token_payload = {
+        "user_id": user.id,
+        "exp": datetime.datetime.utcnow() +
+        datetime.timedelta(days=5),
+        "iat": datetime.datetime.utcnow(),
+    }
+    refresh_token = jwt.encode(
+        refresh_token_payload, settings.SECRET_KEY, algorithm="HS256"
+    )
+    return refresh_token
---
+++ auth_system/authentication/custom_auth/verify.py
@@ -0,0 +1,67 @@
+"""
+Module for defining custom authentication
+"""
+
+import jwt
+from rest_framework.authentication import BaseAuthentication
+from django.middleware.csrf import CsrfViewMiddleware
+from rest_framework import exceptions
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+USER = get_user_model()
+
+
+class CSRFCheck(CsrfViewMiddleware):
+
+    def _reject(self, request, reason):
+        return reason
+
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        """
+        Decoding the user payload and authenticating user from the db
+        """
+
+        authorization_header = request.headers.get('Authorization')
+
+        if not authorization_header:
+            return None
+
+        try:
+            # getting the encoded access_token, sperated by a space (' ')
+            auth_token = authorization_header.split(" ")[1]
+
+            # decoding the encoded payload to get user details
+            payload = jwt.decode(
+                auth_token, settings.SECRET_KEY, algorithms=["HS256"])
+
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed("Access Token expired")
+
+        except IndexError:
+            raise exceptions.AuthenticationFailed("Token prefix missing")
+
+        # getting the user and checking for its presence
+        user = USER.objects.filter(id=payload["user_id"]).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed("User not found")
+        if not user.is_active:
+            raise exceptions.AuthenticationFailed("User is inactive")
+
+        self.enforce_csrf(request)
+        return user, None
+
+    def enforce_csrf(self, request):
+        """
+        Since django-rest-framwork enforces CSRF only in the
+        session authentication, we need to ensure that it enforces CSRF
+        for API views as well using the @ensure_csrf_cookie decorator
+        """
+        check = CSRFCheck()
+        check.process_request(request)
+        reason = check.process_view(request, None, (), {})
+
+        if reason:
+            raise exceptions.PermissionDenied("CSRF Failed: %s" % reason)
